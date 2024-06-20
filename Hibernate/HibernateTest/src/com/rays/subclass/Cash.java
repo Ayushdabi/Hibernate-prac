02b1ca9ce8b637a8b4cc157640169f8bf7f2d5d3
Ayushdabi <udaydabi7@gmail.com>
2024-06-20 13:00:23 +0530

    done

--- Hibernate/HibernateTest/src/com/rays/subclass/Cash.java
+++
@@ -1,7 +0,0 @@
-package com.rays.subclass;
-
-import com.rays.inheritance.Payment;
-
-public class Cash  extends Payment{
-
-}
--- Hibernate/HibernateTest/src/com/rays/subclass/discriminator/Cash.java
+++
@@ -1,7 +0,0 @@
-package com.rays.subclass.discriminator;
-
-import com.rays.inheritance.Payment;
-
-public class Cash extends Payment {
-
-}
